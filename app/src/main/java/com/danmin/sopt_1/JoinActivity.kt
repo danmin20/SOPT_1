e873d1ed94d4e68529253dd0bd877a75739ec87a
leejeongmin <danmin20@khu.ac.kr>
2020-04-20 18:10:24 +0900

    activities

--- app/src/main/java/com/danmin/sopt_1/JoinActivity.kt
+++ app/src/main/java/com/danmin/sopt_1/JoinActivity.kt
@@ -1,12 +1,36 @@
 package com.danmin.sopt_1
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_join.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class JoinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        join_btn.setOnClickListener {
+            if (join_email.text.isNullOrBlank() || join_pw.text.isNullOrBlank()) {
+                Toast.makeText(this, "이메일과 비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+            } else if (join_check_pw.text.toString() != join_pw.text.toString()) {
+                Toast.makeText(this, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.putExtra("email", join_email.text.toString())
+                intent.putExtra("password", join_pw.text.toString())
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+        }
+        gotoLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/danmin/sopt_1/LoginActivity.kt
+++ app/src/main/java/com/danmin/sopt_1/LoginActivity.kt
@@ -1,12 +1,46 @@
 package com.danmin.sopt_1
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_btn.setOnClickListener {
+            if (login_email.text.isNullOrBlank() || login_pw.text.isNullOrBlank()) {
+                Toast.makeText(this, "이메일과 비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivityForResult(intent, 100)
+            }
+        }
+        gotoJoin.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            Log.d("tag",requestCode.toString())
+            when (requestCode){
+                100 -> {
+                    val savedEmail = data!!.getStringExtra("email").toString()
+                    login_email.setText(savedEmail)
+                    val savedPw = data!!.getStringExtra("password").toString()
+                    login_pw.setText(savedPw)
+                }
+            }
+        }
     }
 }
